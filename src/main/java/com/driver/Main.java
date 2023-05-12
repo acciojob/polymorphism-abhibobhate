e8821a79ecde358f20beddeb99b63108ec36e97b
abhibobhate <95496264+abhithor@users.noreply.github.com>
2023-05-12 14:25:49 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p=new Product();
 
+        System.out.println(p.product(1,2));
+
+        System.out.println(p.product(1,2,3));
+
+        double x=1.0,y=2.0;
+
+        System.out.println(p.product(x,y));
+    }
 }
